4da2c04ec0759660f226f96a1fa551592e9138d5
ohmono <basura1300@gmail.com>
2021-08-27 20:07:11 -0500

    added sucursal functions

--- backend/controllers/sucursal.controller.js
+++ backend/controllers/sucursal.controller.js
@@ -1,33 +1,11 @@
-// import JSON management functions
-const { saveAccountData,
-    getAccountData } = require("../DB/control.DB");
+// import JSON management instance
+const { DBController } = require("../DB/DB.controller");
 
-// get all sucursals id
-function view_all_sucursals(req, res) {
-    var sucursal_list = Object.keys(getAccountData("sucursals.json"));
-    res.send(sucursal_list);
+// get all sucursals ids and cities
+function get_all_sucursals(req, res) {
+    var sucursal_list = DBController.getData("sucursals.json");
+    var sucursals = Object.keys(sucursal_list).map(sucursal_id => ({ "sucursal_id": sucursal_id, "city": sucursal_list[sucursal_id]["city"] }))
+    res.send({ "sucursals": sucursals });
 }
 
-// get all cashiers by sucursal id
-function view_all_cashiers(req, res) {
-    var sucursal_id = req.params.id;
-    var cashiers = getAccountData("sucursals.json")[sucursal_id]["cashiers"];
-
-    res.send(cashiers);
-}
-
-function store_sucursal_cashier(req, res) {
-    var current_log = getAccountData("current_sucursal_cashier.json")
-    current_log["data"] = req.body
-    saveAccountData(current_log, "current_sucursal_cashier.json")
-    res.send(current_log["data"])
-}
-
-function get_current_sucursal_cashier(req, res) {
-    var current = getAccountData("current_sucursal_cashier.json")["data"]
-    res.send(current)
-}
-
-
-
-module.exports = { view_all_sucursals, view_all_cashiers, store_sucursal_cashier, get_current_sucursal_cashier };
+module.exports = { get_all_sucursals };
