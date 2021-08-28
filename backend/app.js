6271ba8d86868bc92d426c4d9cc967fb6e169c86
ohmono <basura1300@gmail.com>
2021-08-27 20:05:31 -0500

    add some comments

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,23 @@
-
+// import the backend request receiver
 const cors = require('cors');
 // import the backend server creator
 const express = require("express");
 // import the parser module for json request and response bodys 
 const bodyParser = require("body-parser");
 
-
 // create the server
 const app = express();
 
+// apply request receiver
 app.use(cors())
 // apply parse json
 app.use(bodyParser.json());
-// apply the parse url encoded request bodys
-// app.use(bodyParser.urlencoded({ extended: true }));
 
 // add the server routes 
 const routes = require("./Router/router");
 app.use("/", routes);
 
 // start the server
-app.listen(3000, () => {
-    console.log("listening at port:3000")
+app.listen(PORT = 3000, () => {
+    console.log(`Backend listening on URL: http://[::1]:${PORT}`)
 })
