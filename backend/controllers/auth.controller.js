34ecba85196ddff4ec57cb73ae448af0e87e9a14
ohmono <basura1300@gmail.com>
2021-08-29 23:31:31 -0500

    fis auth services

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,14 +4,14 @@ const { DBController } = require("../DB/DB.controller");
 // set selected sucursal and cashier
 function set_current_cashier(req, res) {
     var current_log = DBController.getData("current_sucursal_cashier.json")
-    current_log["data"] = req.body
+    current_log = req.body
     DBController.saveData(current_log, "current_sucursal_cashier.json")
-    res.send(current_log["data"])
+    res.send(current_log)
 }
 
 // get the current logged cashier
 function get_current_cashier(req, res) {
-    var current = DBController.getData("current_sucursal_cashier.json")["data"]
+    var current = DBController.getData("current_sucursal_cashier.json")
     res.send(current)
 }
 
