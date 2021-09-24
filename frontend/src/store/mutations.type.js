653ec5883d1ea6df78836b3af13a11b4a2692544
Juan Fernando Cardona <juan.cardona32@udea.edu.co>
2021-09-23 21:23:45 -0500

    add mutations for global state

---
+++ frontend/src/store/mutations.type.js
@@ -0,0 +1,9 @@
+export const SET_AUTH = "setUser";
+export const PURGE_AUTH = "logOut";
+export const SET_ERROR = "setError";
+
+export const SET_SUCURSALS = "setSucursals";
+export const SET_CASHIERS = "setCashiers";
+
+export const SET_CLIENT = "setClient";
+export const RESET_CLIENT = "resetClient";
