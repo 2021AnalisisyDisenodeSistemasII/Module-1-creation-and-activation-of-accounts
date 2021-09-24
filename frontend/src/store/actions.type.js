62ba8ee75b7c1c26eeac6cf3b238e46967315a6c
Juan Fernando Cardona <juan.cardona32@udea.edu.co>
2021-09-23 21:23:29 -0500

    add action const for global state

---
+++ frontend/src/store/actions.type.js
@@ -0,0 +1,11 @@
+// auth module const
+export const LOGIN = "login";
+export const LOGOUT = "logout";
+
+export const GET_SUCURSALS = "getSucursals";
+export const GET_CASHIERS = "getCashiers";
+
+export const GET_CLIENT = "getClient";
+export const PURGUE_CLIENT = "purgueClient";
+
+export const CREATE_ACCOUNT = "createAccount";
