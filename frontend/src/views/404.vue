cb42b4a7f9d7f6a97ee59d2b5e4d7fa315c4f910
Juan Fernando Cardona <juan.cardona32@udea.edu.co>
2021-09-23 21:24:33 -0500

    add some prevention error and security views

---
+++ frontend/src/views/404.vue
@@ -0,0 +1,43 @@
+<template>
+  <transition
+    appear
+    name="fab-transition"
+    :duration="{ enter: 500, leave: 800 }"
+    ><v-container fluid fill-height v-show="show">
+      <v-card class="mx-auto" width="535px" align="center"
+        ><v-row>
+          <v-col cols="6">
+            <v-img
+              src="../assets/img/error-404.png"
+              height="250px"
+              contain
+            ></v-img>
+          </v-col>
+          <v-col align-self="center">
+            <v-card-title class="justify-center">
+              Oops: ERROR 404 Page Not Found
+            </v-card-title>
+
+            <v-card-subtitle class="justify-center">
+              El mono made something wrong
+            </v-card-subtitle>
+            <v-card-text class="justify-center"></v-card-text>
+          </v-col>
+        </v-row>
+      </v-card> </v-container
+  ></transition>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    show: false,
+  }),
+  mounted() {
+    this.show = !this.show;
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ frontend/src/views/Error.vue
@@ -0,0 +1,33 @@
+<template>
+  <transition
+    appear
+    name="fab-transition"
+    :duration="{ enter: 500, leave: 800 }"
+    ><v-container fluid fill-height>
+      <v-card class="mx-auto" width="535px" align="center"
+        ><v-row>
+          <v-col cols="6">
+            <v-img src="../assets/img/aaah.gif" height="250px" contain></v-img>
+          </v-col>
+          <v-col align-self="center">
+            <v-card-title class="justify-center">
+              AH AH AH! <br />
+              You Don't Have Access
+            </v-card-title>
+
+            <v-card-subtitle class="justify-center">
+              Log in first
+            </v-card-subtitle>
+            <v-card-text class="justify-center"></v-card-text>
+          </v-col>
+        </v-row>
+      </v-card> </v-container
+  ></transition>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
