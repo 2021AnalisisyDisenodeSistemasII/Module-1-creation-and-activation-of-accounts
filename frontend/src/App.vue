759c825aa6c1bed0b0708dc5307d7cd17f87b081
Juan Fernando Cardona <juan.cardona32@udea.edu.co>
2021-09-23 21:25:13 -0500

    reduce code sice

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,59 +1,25 @@
 <template>
   <div>
-    <header>
-      <div v-if="$route.name.indexOf('Services') !== -1">
-        <router-view />
-      </div>
-      <div v-else>
-        <div id="nav">
-          <router-link to="/">Home</router-link>
-        </div>
-        <router-view />
-      </div>
-    </header>
+    <v-app>
+      <Navigator></Navigator>
 
-    <footer>
-      <div>
-        <footer class="footer-dark">
-          <div class="container">
-            <div class="row">
-              <div class="col item social">
-                <a
-                  href="https://github.com/2021AnalisisyDisenodeSistemasII/Module-1-opening-and-activation-of-accounts"
-                  ><i class="icon ion-social-github"></i
-                ></a>
-              </div>
-            </div>
-            <p class="copyright">
-              Module 1: Opening and activation of accounts © 2021
-            </p>
-          </div>
-        </footer>
-      </div>
-    </footer>
+      <v-main> <router-view /> </v-main>
+
+      <Footer></Footer>
+    </v-app>
   </div>
 </template>
 
+<script>
+import Footer from "./components/Footer.vue";
+import Navigator from "./components/Navigator.vue";
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+export default {
+  components: { Navigator, Footer },
+  name: "App",
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #e43c5c;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
