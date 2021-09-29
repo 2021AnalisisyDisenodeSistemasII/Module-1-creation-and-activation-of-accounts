1171f66ac94b6369a9a37659978c0d2f424dcf00
Juan Fernando Cardona <juan.cardona32@udea.edu.co>
2021-09-29 11:30:34 -0500

    created check auth action

--- frontend/src/store/actions.type.js
+++ frontend/src/store/actions.type.js
@@ -1,6 +1,7 @@
 // auth module const
 export const LOGIN = "login";
 export const LOGOUT = "logout";
+export const CHECK_AUTH = "checkAuth";
 
 export const GET_SUCURSALS = "getSucursals";
 export const GET_CASHIERS = "getCashiers";
